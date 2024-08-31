349d8d8da3756faa047ffbb3c7520e2f85faf830
Lee ChanMi <anytime0224@gmail.com>
2024-08-31 12:26:03 +0900

    #7 [refactor] 생성자를 필수 생성자 어노테이션으로 대체

--- src/main/java/com/tenten/eatmatjib/common/util/CoordinateConverter.java
+++ src/main/java/com/tenten/eatmatjib/common/util/CoordinateConverter.java
@@ -1,15 +1,13 @@
 package com.tenten.eatmatjib.common.util;
 
 import com.tenten.eatmatjib.member.domain.Coord;
+import lombok.RequiredArgsConstructor;
 import org.locationtech.proj4j.CoordinateTransform;
 import org.locationtech.proj4j.ProjCoordinate;
 
+@RequiredArgsConstructor
 public class CoordinateConverter {
     private final CoordinateTransform coordinateTransform;
-
-    public CoordinateConverter(CoordinateTransform coordinateTransform) {
-        this.coordinateTransform = coordinateTransform;
-    }
 
     public Coord convert(Coord coord) {
         ProjCoordinate result = new ProjCoordinate();
