16e60bfae097f5f0b99a7938276ade8603a436ca
hyunkyung <sybaik98@gmail.com>
2024-09-02 14:36:49 +0900

    #2 [etc] 코드리뷰 반영 - swagger 설명 에러응답 부분 수정

--- src/main/java/com/tenten/eatmatjib/restaurant/controller/RestaurantController.java
+++ src/main/java/com/tenten/eatmatjib/restaurant/controller/RestaurantController.java
@@ -1,11 +1,14 @@
 package com.tenten.eatmatjib.restaurant.controller;
 
 
+import com.tenten.eatmatjib.common.exception.ErrorResponse;
 import com.tenten.eatmatjib.restaurant.domain.Restaurant;
 import com.tenten.eatmatjib.restaurant.dto.RestaurantQueryRes;
 import com.tenten.eatmatjib.restaurant.service.RestaurantQueryService;
 import com.tenten.eatmatjib.review.domain.Review;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,7 +31,7 @@ public class RestaurantController {
       description = "요청받은 restaurantId의 음식점에 대해 상세정보(모든필드)를 반환합니다.",
       responses = {
           @ApiResponse(responseCode = "200", description = "음식점의 모든 필드를 포함하는 DTO 반환"),
-          @ApiResponse(responseCode = "404", description = "음식점이 존재하지 않습니다."),
+          @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}),
       }
   )
   public ResponseEntity<RestaurantQueryRes> getRestaurantDetail(@PathVariable("restaurantId") Long id) {
